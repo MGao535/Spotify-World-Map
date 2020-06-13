412f1f3831944fa7eff96bd23363648422771aea
MGao535 <mmg535@nyu.edu>
2020-06-13 12:52:10 -0500

    added map markers

--- Shawn/test/web-api-auth-examples-master/authorization_code/public/dashboard/static/js/logic.js
+++ Shawn/test/web-api-auth-examples-master/authorization_code/public/dashboard/static/js/logic.js
@@ -4,6 +4,10 @@ var myMap = L.map("map", {
   zoom: 3
 });
 
+let country = [];
+let lats = [];
+let longs = [];
+
 // Adding tile layer to the map
 L.tileLayer(MAP_URL, {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -12,6 +16,21 @@ L.tileLayer(MAP_URL, {
   accessToken: API_KEY
 }).addTo(myMap);
 
-d3.csv("LatLong.txt").then(function(data){
-  console.log(data);
-})
+d3.csv("/static/js/LatLong.txt", function(data) {
+	console.log(data);
+    for(var i = 0; i < data.length; i++){
+    	//console.log(data[i].Latitude);
+    	country.push(data[i].Country);
+    	lats.push(data[i].Latitude);
+    	longs.push(data[i].Longitude);
+    }
+    //console.log(lats);
+
+    for(var j = 0; j < lats.length; j++){
+	var latlng = L.latLng(lats[j], longs[j]);
+	L.marker(latlng)
+	.bindPopup("<a href = " + data[j].Playlist + ">" + country[j] + "</a>")
+	.addTo(myMap);
+};
+});
+
