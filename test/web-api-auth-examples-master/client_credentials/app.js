c9fb6eab687943cf5f2a66969ebeec1dc6066152
Sguthrie123 <42792003+Sguthrie123@users.noreply.github.com>
2020-06-14 21:22:38 -0400

    got a call

--- test/web-api-auth-examples-master/client_credentials/app.js
+++ test/web-api-auth-examples-master/client_credentials/app.js
@@ -9,14 +9,14 @@
 
 var request = require('request'); // "Request" library
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
+var client_id = 'd76e0263612746c1891673bca5220cd0'; // Your client id
+var client_secret = '7c34c892933d446fae874d28b3f6a9b2'; // Your secret
 
 // your application requests authorization
 var authOptions = {
   url: 'https://accounts.spotify.com/api/token',
   headers: {
-    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
   },
   form: {
     grant_type: 'client_credentials'
@@ -30,7 +30,7 @@ request.post(authOptions, function(error, response, body) {
     // use the access token to access the Spotify Web API
     var token = body.access_token;
     var options = {
-      url: 'https://api.spotify.com/v1/users/jmperezperez',
+      url: 'https://api.spotify.com/v1/audio-features/6In6SkveIw26thdYRhHTVX',
       headers: {
         'Authorization': 'Bearer ' + token
       },
@@ -39,5 +39,8 @@ request.post(authOptions, function(error, response, body) {
     request.get(options, function(error, response, body) {
       console.log(body);
     });
+
+    
   }
 });
+
